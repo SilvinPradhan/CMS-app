1f8e69aa06e2792d93669a99a94604dde89ab697
Silvin Pradhan <silvinpradhan95@gmail.com>
2020-12-17 16:50:56 -0600

    use of useQuery and useLazyQuery from apollo graph and moved the home component to pages folder

--- gql/client/src/App.js
+++ gql/client/src/App.js
@@ -1,44 +1,21 @@
-import React, { useState } from "react";
-import ApolloClient, { gql } from "apollo-boost";
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+// Components
+
+// Pages
+import Home from "../../client/src/pages/home/home.components";
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
 });
 
 const App = () => {
-  const [post, setPosts] = useState([]);
-  client
-    .query({
-      query: gql`
-        {
-          allPosts {
-            id
-            title
-            description
-          }
-        }
-      `,
-    })
-    .then((result) => setPosts(result.data.allPosts));
   return (
-    <>
-      <div className="container">
-        <div className="row p-5">
-          {post.map((posts) => (
-            <div className="col-md-4" key={posts.id}>
-              <div className="card">
-                <div className="card-body">
-                  <div className="card-title">
-                    <h4>{posts.title}</h4>
-                  </div>
-                  <p className="card-text">{posts.description}</p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
+    // Provide "client", pass the "client" as props below
+    <ApolloProvider client={client}>
+      <Home />
+    </ApolloProvider>
   );
 };
 
